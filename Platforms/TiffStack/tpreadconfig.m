49492be03abf0389a66271bd4b0c469e7ead96f9
Steve Van Hooser <vanhoosr@brandeis.edu>
2017-01-16 16:05:59 -0500

    update tiffstack support to multiple files per directory

--- Platforms/TiffStack/tpreadconfig.m
+++ Platforms/TiffStack/tpreadconfig.m
@@ -11,7 +11,10 @@ function params=tpreadconfig( tpdirname )
 % FrameRate<tab>VALUE<tab>The frame rate in Hz
 % 
 % The directory will be examined for a stack of TIFFS. If one is
-% found, then the image size will be determined from this file.
+% found, then the image size will be determined from this file. If
+% multiple TIFF files are found, then it is assumed that they
+% are sequential in alphabetical order, and that the last frame
+% in one file occurs one frame before the first frame in the next file.
 %
 % The following parameters will be generated. For the purposes of
 % importing generic TIFF stacks, it will be assumed
@@ -26,12 +29,9 @@ function params=tpreadconfig( tpdirname )
 %  params.Main.Pixels_per_line  (number of pixels per line)
 %  params.Image_TimeStamp__us_  (list of all frame timestamps)
 %  params.Cycle_N.Number_of_images (num. of images in Cycle N)
+%  params.Image_TimeStamp__s_   = params.Image_TimeStamp__us_ * 1E-6
 %
-%   params.Image_TimeStamp__s_   = params.Image_TimeStamp__us_ * 1E-6
-%
-% Inscopix version
-%
-% 2016, SDV
+% 2016-7, SDV
 %
 
 
@@ -51,7 +51,9 @@ for i=1:length(extensionlist),
 	if isempty(datafile),
 		%error(['No data file *.tif in directory ' tpdirname '.']);
 	elseif length(datafile)>1,
-		error(['Multiple data files *.' extensionlist{i} ' in directory ' tpdirname '. Do not know what to do.']);
+		foundit = 1;
+		break;
+		%error(['Multiple data files *.' extensionlist{i} ' in directory ' tpdirname '. Do not know what to do.']);
 	else,
 		foundit = 1;
 		break;
@@ -72,9 +74,10 @@ end;
 
 tiffstackparams = rmfield(tiffstackparams,'dummy');
 
+   % we assume the TIF files in the directory all have the same parameters
 theimfinfo = imfinfo([tpdirname filesep datafile(1).name]);
 
-params.Main.Total_cycles = 1;
+params.Main.Total_cycles = length(datafile);
 params.Main.Scanline_period__us_ = 1;
 params.Main.Dwell_time__us_ = params.Main.Scanline_period__us_ / theimfinfo(1).Width;
 params.Main.Lines_per_frame = theimfinfo(1).Height;
@@ -84,10 +87,11 @@ params.Main.Frame_period__us_ = 1e6 * (1/(tiffstackparams.FrameRate));
 framecount = 0;
 for i=1:params.Main.Total_cycles,
 	cyclename = ['Cycle_' int2str(i)];
+	theimfinfo = imfinfo([tpdirname filesep datafile(i).name]);
 	num_of_images = length(theimfinfo);
 	params = setfield(params,cyclename,...
 		struct('Number_of_images', num_of_images, ...
-			'filename', datafile(1).name, ...
+			'filename', datafile(i).name, ...
 			'imfinfo',theimfinfo));
 	framecount = framecount + num_of_images;
 
