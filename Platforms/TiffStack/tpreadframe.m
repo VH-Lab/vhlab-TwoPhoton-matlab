a41fd2a38cca289edd8f3715338562da89772bb9
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2017-07-12 14:03:03 -0400

    twophoton update

--- Platforms/TiffStack/tpreadframe.m
+++ Platforms/TiffStack/tpreadframe.m
@@ -9,7 +9,4 @@ fname=fullfile(dirname,tpfilename(fnameprefix,cycle,channel,frame));
 
 cycle_string = ['Cycle_' int2str(cycle)];
 
-fname,
-getfield(getfield(fnameprefix,cycle_string),'imfinfo')
-
 im=imread(fname,frame,'info',getfield(getfield(fnameprefix,cycle_string),'imfinfo'));
