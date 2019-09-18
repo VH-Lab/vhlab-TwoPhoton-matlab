ed9c8f3d6c8a11efd4617624d7af6ecf34ae61c2
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2019-09-18 10:55:35 -0400

    updated with better error messages

--- converters/sandrine2vhlab/sandrine2vhlabtstack_dir.m
+++ converters/sandrine2vhlab/sandrine2vhlabtstack_dir.m
@@ -8,17 +8,36 @@ function sandrine2vhlabtstack_dir(dirname)
 
 d = dir([dirname filesep '*.lif']);
 
+if isempty(d),
+	error(['Found no *.lif files in directory ' dirname '.']);
+end;
+
 for i=1:numel(d),
-	datestring = ['20' d(i).name(1:2) '-' d(i).name(3:4) '-' d(i).name(5:6)];
-	
-	rest = d(i).name(7:end-4);
+	if strncmp(d(i).name,'20',2),
+		% it has the 20 out front
+		datestring = [d(i).name(1:4) '-' d(i).name(5:6) '-' d(i).name(7:8)];
+		rest = d(i).name(9:end-4);
+	else, % it doesn't
+		datestring = ['20' d(i).name(1:2) '-' d(i).name(3:4) '-' d(i).name(5:6)];
+		rest = d(i).name(7:end-4);
+	end;
 
 	dash = find(rest=='-');
 
-	genotype = rest(1:dash(1)-1);
+	if isempty(dash),
+		genotype = 'GT';
+	else,
+		genotype = rest(1:dash(1)-1);
+	end;
+
+	if isempty(dash),
+		condition_string = rest;
+	else,
+		condition_string = rest(dash(1)+1:end);
+	end;
 
 	parentdir = [dirname filesep genotype];
-	conditiondir = [parentdir filesep rest(dash(1)+1:end)];
+	conditiondir = [parentdir filesep condition_string];
 	fulldir = [conditiondir filesep datestring];
 
 	try,
