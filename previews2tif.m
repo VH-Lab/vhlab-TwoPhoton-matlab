634c658b1b79b9ec072b44abd3a090183a171382
Steve Van Hooser <vanhoosr@brandeis.edu>
2020-10-27 09:52:02 -0400

    updated previews2tif for new previewimage formats

--- previews2tif.m
+++ previews2tif.m
@@ -1,29 +1,52 @@
-function previews2tif(dirname)
+function previews2tif(ds, dirname,scale,map)
 
 % PREVIEWS2TIF - Convert all preview images in a 2P directory to TIF
 %
-%  PREVIEWS2TIF(DIRNAME)
+%  PREVIEWS2TIF(DS, DIRNAME [, SCALE, MAP])
+%
+%  DS is a directory structure DIRSTRUCT object.
 %
 %  The files will be written in the DIRNAME/analysis/scratch directory.
 %
-%  All files will be scaled by their minimum and maximal pixel values.
+%  All files will be scaled by their minimum and maximal pixel values
+%  unless SCALE is given; then images will be scaled between SCALE(1)
+%  and SCALE(2).
+%  
+%  If no MAP is given, then the gray color map of 256 entries is used. Otherwise,
+%  MAP is used. 
 %
 
-ds = dirstruct(dirname);
+if nargin<3,
+    scale = [];
+end;
+
+if nargin<4,
+    map = gray(256);
+end;
 
 sd = getscratchdirectory(ds);
 
+dirname_fullpath = [getpathname(ds) filesep dirname filesep];
+
 if ~isempty(sd),
-	d = dir([sd filesep 'preview*.mat']);
+	d = dir([dirname_fullpath 'tppreview*.mat']);
 	for i=1:length(d),
-		if ~eqlen(d(i).name,'..')&~eqlen(d(i).name,'.'),
-			g = load([sd filesep d(i).name]);
-			[pathname,filename] = fileparts(d(i).name);
-			g.pvimg = double(g.pvimg);
-			img = rescale(g.pvimg,[min(g.pvimg(:)) max(g.pvimg(:))],[0 255]);
-			imwrite(uint8(img),[sd filesep d(i).name '.tiff'],'tiff');
-		end;
+        g = load([dirname_fullpath filesep d(i).name]);
+        [pathname,filename] = fileparts(d(i).name);
+        g.pvimg = double(g.pvimg);
+        if isempty(scale),
+            scale_here = [min(g.pvimg(:)) max(g.pvimg(:))];
+        else,
+            scale_here = scale;
+        end;
+        img = rescale(g.pvimg,scale_here,[0 size(map,1)-1]);
+        if size(map,1)>2^16,
+            img_ = uint32(img);
+        elseif size(map,1)>2^8,
+            img_ = uint16(img);
+        else,
+            img_ = uint8(img);
+        end;
+        imwrite(img_,map,[sd filesep dirname '_' d(i).name '.tif']);
 	end;
 end;
-
-
