44b89c6bd3cb5052ee2e1d4ca0b8e26edf4c428e
Steve Van Hooser <vanhoosr@brandeis.edu>
2024-01-31 07:38:06 -0500

    ndi exporting

--- converters/to_ndi/stack2ndi.m
+++ converters/to_ndi/stack2ndi.m
@@ -6,56 +6,109 @@ function b = stack2ndi(tpstack_file, S, subjectname)
 % Given a stack file TPSTACK_FILE and an ndi.session S, and the subjectname,
 % export the subject and neurons.
 %
+% Needs to have subject.txt file in folder (e.g., 'scn1-722@turrigiano.org')
+%
 % Example: 
 %  tpstack_file = '/Volumes/van-hooser-lab/Users/Diane/For_Diane/2722-05-05/analysis/scratch/stack1.stack';
+%  S = ndi.setup.vhlab('2722','/Users/vanhoosr/Desktop/2722-05-05');
 %  S = ndi.session.dir('/Volumes/van-hooser-lab/Users/Diane/For_Diane/2722-05-05');
 %  b = stack2ndi(tpstack_file,S);
 %
 %
 
+[tppath,sitename,ext] = fileparts(tpstack_file);
+
 tpstack = load(tpstack_file,'-mat');
 
-  % Step 1, make sure we have set up the subject
-
-s_q = ndi.query('subject.name','exact_string',subjectname);
+subjectname = vlt.file.textfile2char([S.path() filesep 'subject.txt']);
+s_q = ndi.query('base.name','exact_string',subjectname) & ndi.query('','isa','subject');
 
 s = S.database_search(s_q);
 
 if isempty(s),
-	su = ndi.subject(subjectname,'');
-	subdoc = su.newdocument() + S.newdocument();
-	keyboard; % check to make sure session is on board
-	
-
-	S.database_add(subdoc);
-else,
-	subdoc = s{1};
+	error(['Oh, I didn't find a subject.']);
 end;
 
+subdoc = s{1};
 subdoc_id = subdoc.id();
 
  % get visual stimulator here
 
-srp = ndi.document('stimulus_response_scalar_parameters_basic') + S.newdocument();
+decoder = ndi.app.stimulus.decoder(S);
+rapp = ndi.app.stimulus.tuning_response(S);
 
- % Step 3, add the elements
+p = S.getprobes('type','stimulator');
+if isempty(p), error(['Could not find stimulator.']); end;
+decoder.parse_stimuli(p{1},0);
+cs_doc = rapp.label_control_stimuli(p{1},0);
+
+stimulator_id = p{1}.id();
+
+pres = S.database_search(ndi.query('','isa','stimulus_presentation'));
+
+srp = ndi.document('stimulus_response_scalar_parameters_basic') + S.newdocument();
+srp_struct = srp.document_properties.stimulus_response_scalar_parameters_basic;
+srp_struct.prestimulus_time = 5;
+srp_struct.prestimulus_normalization = 3;
+srp = ndi.document('stimulus_response_scalar_parameters_basic',...
+		'stimulus_response_scalar_parameters_basic',srp_struct) + S.newdocument();
+
+S.database_add(srp);
+
+srp_id = srp.id();
+
+ds = dirstruct(S.path());
+
+T = getalltests(ds);
 
 nde = {};
+nde_id = {};
+cellnames = {};
 
-options.stimulus_response_scalar_paramers_id = srp.id();
+for t=1:numel(T),
 
-stim_response_doc = {};
-
-for i=1:numel(tpstack.cellllist),
-
-	nde{i} = ndi.element.timeseries(S,['cell ' tpstack.celllist(i).name],1,'roi',[],0,subdoc_id);
-
-	options.element_id = ndi{i}.id();
-
-	stim_response_doc{i} = stackresp2ndiresponsedoc(resp_struct,nde{i});
-
-	S.database_add(stim_response_doc{i});
+	% Step 1: add all new cells as ndi.elements
+if 0,
+	for i=1:numel(tpstack.celllist),
+		if strcmp(T{t},tpstack.celllist(i).dirname),
+			cellname_here = ['cell ' int2str(tpstack.celllist(i).index) ' ref ' ...
+				tpstack.celllist(i).dirname]
+			disp(['...making element...']);
+			cellnames{end+1} = cellname_here;
+			nde{end+1} = ndi.element.timeseries(S,cellname_here, 1,'roi',[],0,subdoc_id);
+			nde_id{end+1} = nde{end}.id();
+		end;
+	end;
 end;
 
+	%which stim_pres do we have?
 
+	stim_index = [];
+	for pr = 1:numel(pres),
+		if strcmp(pres{pr}.document_properties.epochid.epochid,T{t}),
+			stim_index = pr;
+			break;
+		end;
+	end;
+	control_stim_index = [];
+	for pr = 1:numel(cs_doc),
+		if strcmp(cs_doc{pr}.dependency_value('stimulus_presentation_id'),pres{stim_index}.id()),
+			control_stim_index = pr;
+			break;
+		end;
+	end;
+
+	% load responses, add responses from all cells recorded here
+
+	resp_struct = load([getpathname(ds) filesep T{t} filesep sitename '_' T{t} '.mat']);
+
+	stim_response_doc = {};
+	for i=1:numel(resp_struct.listofcellnames),
+		disp(['Working on cell ' int2str(i) ' of ' int2str(numel(resp_struct.listofcellnames))])
+		index = find(strcmp(resp_struct.listofcellnames{i},cellnames));
+		stim_response_doc{i} = stackresp2ndiresponsedoc(S,T{t},resp_struct.resps(i),...
+			pres{stim_index},cs_doc{control_stim_index},nde_id{index},stimulator_id,srp_id);
+		S.database_add(stim_response_doc{i});
+	end;
+end;
 
--- converters/to_ndi/stackresp2ndiresponsedoc.m
+++ converters/to_ndi/stackresp2ndiresponsedoc.m
@@ -1,42 +1,60 @@
-function [stim_response_doc] = stackresp2ndiresponsedoc(resp_struct, nde, options)
+function [stim_response_doc] = stackresp2ndiresponsedoc(S, tdir, resp_struct, stim_presentation, control_stim_presentation, nde_id, stimulator_id, srp_id)
 %
 % STIM_RESP_DOC = STACKRESP2NDIRESPONSEDOC(RESP_STRUCT)
 %
 
 stim_response_doc = [];
 
-index = find(strcmp(nde.name,resp_struct.list_of_cellnames));
-
-responses = vlt.data.empty_struct('stimid','response_real','response_imaginary',...
+responses = vlt.data.emptystruct('stimid','response_real','response_imaginary',...
 	'control_response_real','control_response_imaginary');
 
-if ~isempty(index),
-	for i=1:numel(resp_struct.resps(index).ind),
-		response_here = [];
-		response_here.stimid = i;
-		response_here.response_real = resp_struct.resps(index).ind{i};
+all_stims = unique(stim_presentation.document_properties.stimulus_presentation.presentation_order);
+
+stim_counters = zeros(numel(all_stims),1);
+blank_counter = 0;
+
+c_stim_id = control_stim_presentation.document_properties.control_stimulus_ids.control_stimulus_ids;
+
+for i=1:numel(stim_presentation.document_properties.stimulus_presentation.presentation_order),
+	response_here = [];
+	response_here.stimid = stim_presentation.document_properties.stimulus_presentation.presentation_order(i);
+	j = find(response_here.stimid == all_stims);
+	if j<numel(all_stims), % regular stimulus
+		stim_counters(j) = stim_counters(j)+1;
+		response_here.response_real = resp_struct.ind{j}(stim_counters(j));
 		response_here.response_imaginary = 0;
-		response_here.control_response_real = resp_struct.resps(index).blankind(i);
-		response_here.control_response_imaginary = 0;
-		responses(end+1) = response_here;
+	else,
+		blank_counter = blank_counter + 1;
+		response_here.response_real = resp_struct.blankind(blank_counter);
+		response_here.response_imaginary = 0;
 	end;
+
+	response_here.control_response_real = 0; 
+	response_here.control_response_imaginary = 0;
+	responses(end+1) = response_here;
+end;
+
+for i=1:numel(c_stim_id),
+	responses(i).control_response_real = responses(c_stim_id(i)).response_real;
 end;
 
 stimulus_response_scalar.response_type = 'mean';
 stimulus_response_scalar.responses = responses;
 
+stimulus_response_struct.stimulator_epochid = tdir;
+stimulus_response_struct.element_epochid = tdir;
+
 stim_response_doc = ndi.document('stimulus_response_scalar',...
-	'stimulus_response_scalar',stimulus_response_scalar) + S.newdocument();
+	'stimulus_response_scalar',stimulus_response_scalar,...
+	'stimulus_response',stimulus_response_struct) + S.newdocument();
 
-stim_response_doc = stim_response_doc.set_dependency('element_id,options.element_id);
+stim_response_doc = stim_response_doc.set_dependency_value('element_id',nde_id);
 
-stim_response_doc = stim_response_doc.set_dependency('stimulator_id',options.stimulator_id);
+stim_response_doc = stim_response_doc.set_dependency_value('stimulator_id',stimulator_id);
 
+stim_response_doc = stim_response_doc.set_dependency_value('stimulus_response_scalar_parameters_id',...
+	srp_id);
 
-stim_response_doc = stim_response_doc.set_dependency('stimulus_response_scalar_parameters_id',...
-	options.stimulus_response_scalar_parameters_basic);
+stim_response_doc = stim_response_doc.set_dependency_value('stimulus_control_id',control_stim_presentation.document_properties.base.id);
+stim_response_doc = stim_response_doc.set_dependency_value('stimulus_presentation_id',stim_presentation.document_properties.base.id);
 
-
-stim_response_doc = stim_response_doc.set_dependency('stimulus_control_id',options.stimulus_control_id);
-stim_response_doc = stim_response_doc.set_dependency('stimulus_presentation_id',...
-	options.stimulus_presentation_id);
